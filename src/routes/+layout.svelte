bb4f268a4e40b9ee08f17db3e18e2b373b42dcde
Israr Ahmad Khan <karimzai@gmail.com>
2022-09-28 13:57:00 +0200

    Logo fixed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,18 +6,18 @@
 </script>
 
 <svelte:head>
-	<title>AFSTATS</title>
+	<title>AFstats</title>
 </svelte:head>
 
 <div class="border-b md:flex items-end border-slate-200 ">
-	<a class="w-48 my-4 mx-6 inline-block" href="/">
-		<img class="w-48 " alt="AFSTATS - Afghanistan Statisctics" src="/afstats-logo-2.png" />
+	<a class="w-36 my-2 mx-6 inline-block" href="/">
+		<img class="w-36 " alt="AFSTATS - Afghanistan Statisctics" src="/afstats-logo-3.png" />
 	</a>
-	<div class="items-start flex">
-		<a class:bg-slate-100={path=='/'} class="ml-12 px-10 py-6 font-medium inline-block bg-slate-100 hover:bg-slate-50" href="/">
+	<div class="items-start ml-6 md:ml-12 flex">
+		<a class:bg-slate-100={path=='/'} class="px-10 py-4 pt-5 font-medium inline-block bg-slate-100 hover:bg-slate-50" href="/">
 			Home
 		</a>
-		<a class:bg-slate-100={path=='/about'} class="px-10 py-6 font-medium inline-block hover:bg-slate-50" href="/about">
+		<a class:bg-slate-100={path=='/about'} class="px-10 py-4 pt-5 font-medium inline-block hover:bg-slate-50" href="/about">
 			About Us
 		</a>
 	</div>
